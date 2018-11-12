9d16e3b855999ba5b2dc57a37d3636989041d4a3
jean <jean.ducellier.pro@gmail.com>
2018-11-12 12:07:24 +0100

    maj ng update = angular 6.0 --> angular 7

--- src/app/exercice3/exercice3.component.ts
+++ src/app/exercice3/exercice3.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice3.component.css']
 })
 export class Exercice3Component implements OnInit {
-
+  title : string = "Exercice 3";
   constructor() { }
 
   ngOnInit() {
