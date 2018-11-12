2012cff14ced437f00df23185728befd08231d18
jean <jean.ducellier.pro@gmail.com>
2018-11-12 18:37:18 +0100

    ajout exercice 4

--- src/app/exercice3/exercice3.component.ts
+++ src/app/exercice3/exercice3.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-exercice3',
   templateUrl: './exercice3.component.html',
   styleUrls: ['./exercice3.component.css']
 })
 export class Exercice3Component implements OnInit {
-  title : string = "Exercice 3";
+
+  public statut : boolean = true ;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/exercice4/exercice4.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-exercice4',
+  templateUrl: './exercice4.component.html',
+  styleUrls: ['./exercice4.component.css']
+})
+export class Exercice4Component implements OnInit {
+  public nombres : Array<{ nombre }> = [];
+  constructor() {
+    this.nombres.push({nombre : "un"});
+    this.nombres.push({nombre : "deux"});
+    this.nombres.push({nombre : "trois"});
+  }
+
+  ngOnInit() {
+  }
+
+}
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ Exercice4Component ],
+  bootstrap: [ Exercice4Component ]
+})
+export class AppModule {}
