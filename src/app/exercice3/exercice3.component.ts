c44e60c958b22aea1dcc3df79085dfd7015663e0
jean <jean.ducellier.pro@gmail.com>
2018-11-13 11:50:24 +0100

    bloquer sur exercice 3

--- src/app/exercice3/exercice3.component.ts
+++ src/app/exercice3/exercice3.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-exercice3',
   templateUrl: './exercice3.component.html',
   styleUrls: ['./exercice3.component.css']
 })
 export class Exercice3Component implements OnInit {
-
+  p1:{};
+  p2:{};
   public statut : boolean = true ;
+  trueOrFalse(event : Event): void{
+    if (this.statut=== true){
+      this.statut = false;
+      console.log(this.statut);
+    }
+    else{
+      this.statut = true;
+
+      console.log(this.statut)
+    }
+  }
   constructor() { }
 
   ngOnInit() {
